b3370e9ebe4dc4dfb0ddd59e13611776aeb662f7
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-21 20:56:07 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,48 +1,9 @@
 const [a, b, c] = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(i=>+i);
-let result;
-if(a===b){
-    if(b===c){
-        // a=b=c
-        result = 10000+a*1000;
-    }
-    else if(b!==c){
-        // a=b
-        result = 1000+a*100;
-    }
-} else if(a!==b){
-    if(b===c){
-        // b=c
-        result = 1000+b*100;
-    }
-    else if(b!==c){
-        if(c===a){
-            // a=c
-            result = 1000+a*100;
-        }
-        else if(c!==a){
-            // 다 다름
-            if(a>b){
-                if(b>c){
-                    result = a*100;
-                }
-                else if(b<c){
-                    if(a>c){
-                        result = a*100;
-                    }
-                    else if(a<c){
-                        result = c*100;
-                    }
-                }
-            }
-            else if(a<b){
-                // 다 다름
-                if(b>c){
-                    result = b*100;
-                } else if(b<c){
-                    result = c*100;
-                }
-            }
-        }
-    }
+if (a === b && b === c && a === c) { console.log(10000 + a * 1000) };
+if (a !== b || b !== c || c !== a) {
+  if (a === b || a === c) { console.log(1000 + a * 100) };
+  if (b === c) { console.log(1000 + b * 100) };
 }
-console.log(result);
+if(a !== b && b !== c && c !== a){
+  console.log([a,b,c].sort().reverse()[0]*100)
+}
