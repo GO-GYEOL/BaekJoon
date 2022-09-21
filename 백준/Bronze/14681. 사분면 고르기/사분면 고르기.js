dc547169c86c775fe63cbbf1e08ee28610adc1e8
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-21 14:30:36 +0900

    Update 사분면 고르기.js

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -10,19 +10,18 @@ rl.on("line", (line)=>{
     input.push(parseInt(line));
 });
 rl.on("close", ()=>{
-    const X = input[0];
-const Y = input[1];
-
-
-if(X>0 && Y>0){
+  const X = input[0];
+  const Y = input[1];
+  
+  if(X>0 && Y>0){
     console.log(1);
-} else if(X>0 && Y<0){
+  } else if(X>0 && Y<0){
     console.log(4);
-} else if(X<0 && Y>0){
+  } else if(X<0 && Y>0){
     console.log(2);
-} else if(X<0 && Y<0){
+  } else if(X<0 && Y<0){
     console.log(3);
-}
+  }
     process.exit();
 });
 
