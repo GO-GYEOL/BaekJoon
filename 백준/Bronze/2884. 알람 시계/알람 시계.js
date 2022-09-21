5a2dea3b260ea565f18126987b2f109036fe26a8
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-21 18:56:03 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -3,15 +3,15 @@ const data = fs.readFileSync('dev/stdin').toString().split(' ').map(i=>+i);
 let hours = data[0];
 let minutes = data[1];
 
+let alarmMinutes = minutes - 45
+    
 if(minutes>=45){
     minutes = minutes-45;
 } else if(minutes<45 && hours != 0){
-    hours = hours-1;
-    const sub = 45 - minutes;
-    minutes = 60 - sub;
+    minutes = alarmMinutes+60;
+    --hours;
 } else if(minutes<45 && hours == 0){
     hours = 23;
-    const sub = 45 - minutes;
-    minutes = 60 - sub;
+    minutes = alarmMinutes+60;
 }
 console.log(hours, minutes);
