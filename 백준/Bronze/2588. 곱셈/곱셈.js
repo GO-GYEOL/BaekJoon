0a9aebe73ee6c0eb414be6225070ac6cb6dd0f9c
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-21 11:25:57 +0900

    Update 곱셈.js

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,17 +1,7 @@
 const fs = require('fs');
-const inputData = fs.readFileSync('dev/stdin').toString().split('\n');
-const num1 = +(inputData[0]);
-const num2 = inputData[1];
-const num2Cut = new Array();
-for(i=0; i<num2.length; i++){
-    num2Cut.push(num2.charAt(i));
-}
-const num3 = num1*parseInt(num2Cut[num2Cut.length-1]);
-const num4 = num1*parseInt(num2Cut[num2Cut.length-2]);
-const num5 = num1*parseInt(num2Cut[num2Cut.length-3]);
-const num6 = num3+(num4*10)+(num5*100);
-
-console.log(num3);
-console.log(num4);
-console.log(num5);
-console.log(num6);
+const data = fs.readFileSync('dev/stdin').toString().split('\n');
+const B = data[1].split("").map(i => +i);
+console.log(data[0]*B[2]);
+console.log(data[0]*B[1]);
+console.log(data[0]*B[0]);
+console.log(data[0]*data[1]);
