11dca1c9e7686cd7e838325c7274315dc2a80c4f
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-23 22:34:03 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9544 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,5 @@
+const [testCase, ...dataArr] = require('fs').readFileSync('dev/stdin').toString().split('\n');
+for(i=0; i<+testCase; i++){
+    const data = dataArr[i].split(' ').map(i => +i);
+    console.log(`Case #${i+1}: ${data[0]} + ${data[1]} = ${data[0]+data[1]}`)
+}
