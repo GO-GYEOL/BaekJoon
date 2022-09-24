13cef6eaf78c11bb762444dbbcfe57ed0b30d4bd
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-24 14:42:11 +0900

    Update X보다 작은 수.js

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,4 +1,4 @@
-// 배열로 출력해도 되나? 아니면 문자열로 해야되나? 배열로 해도 될 듯.
+// filter
 const [info, arr] = require('fs').readFileSync('dev/stdin').toString().split('\n');
 const newArr = arr.split(" ").map((i) => +i);
 const newInfo = info.split(" ").map((i) => +i);
@@ -6,3 +6,16 @@ const newArr2 = newArr.filter((i) => i < newInfo[1]);
 const newArrLast = newArr2.join(' ');
 console.log(newArrLast)
 
+// filter 없이
+const [i, a] = require('fs').readFileSync('dev/stdin').toString().split('\n');
+const info = i.split(" ");
+const infoCnt = +info[0];
+const infoNum = +info[1];
+const arr = a.split(" ").map((i) => +i);
+let msg = "";
+for (let i = 0; i < infoCnt; i++) {
+  if (infoNum > arr[i]) {
+    msg += arr[i].toString()+" ";
+  }
+}
+console.log(msg.trim());
