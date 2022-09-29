6f59dc16c244222154f7c968b18e8cf57913b2b1
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-09-29 21:08:53 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -1,7 +1,12 @@
-const [testCase, ...text] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let score = 0;
-let cnt = 0;
+const [testCase, ...text] = require("fs")
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
 for (i = 0; i < text.length; i++) {
+  let score = 0;
+  let cnt = 0;
   for (j = 0; j < text[i].length; j++) {
     if (text[i][j] === "O") {
       cnt++;
@@ -11,6 +16,4 @@ for (i = 0; i < text.length; i++) {
     }
   }
   console.log(score);
-  score = 0;
-  cnt = 0;
 }
