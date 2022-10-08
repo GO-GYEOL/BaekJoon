22753a050a69373c108097c30f56bf53317a163f
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-10-08 12:36:46 +0900

    Update 알파벳 찾기.js

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -1,15 +1,7 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-
-let resultArr = [];
-let result = '';
-
-for (let i = 97; i <= 122; i++) {
+let input = require('fs').readFileSync('/dev/stdin').toString();
+let result = [];
+for (i = 97; i <= 122; i++) {
   let alphabet = String.fromCharCode(i);
-
-  for (let j = 0; j < input.length; j++) {
-    resultArr.push(input[j].indexOf(alphabet));
-  }
+  result.push(input.indexOf(alphabet));
 }
-
-result = resultArr.join(' ');
-console.log(result);
+console.log(result.join(' '));
