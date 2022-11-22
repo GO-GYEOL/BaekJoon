222bd5c3c62be92e1c497858dc172387be4d9251
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-11-22 10:52:36 +0900

    [Silver III] Title: 소수 구하기, Time: 220 ms, Memory: 24472 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split(' ');
+const start = +input.shift();
+const end = +input.shift();
+const isPrimeNumber = Array(end+1).fill(true);
+isPrimeNumber[0] = isPrimeNumber[1] = false;
+const result = [];
+
+for(let i=2; i<=Math.sqrt(end); i++ ){
+  if(isPrimeNumber[i]){
+    let m = 2;
+    while(i * m <= end){
+      isPrimeNumber[i*m] = false;
+      m++
+    }
+  }
+}
+
+for(let i=start; i<=isPrimeNumber.length; i++){
+  if(isPrimeNumber[i]){
+    result.push(i);
+  }
+}
+console.log(result.join('\n'))
