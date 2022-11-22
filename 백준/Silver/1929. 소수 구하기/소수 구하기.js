1c7f6e5641679a01bd1214bf165b76d004aa3d72
고결 <95490554+GO-GYEOL@users.noreply.github.com>
2022-11-22 11:42:01 +0900

    [Silver III] Title: 소수 구하기, Time: 220 ms, Memory: 24388 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -5,7 +5,7 @@ const isPrimeNumber = Array(end+1).fill(true);
 isPrimeNumber[0] = isPrimeNumber[1] = false;
 const result = [];
 
-for(let i=2; i<=Math.sqrt(end); i++ ){
+for(let i=2; i<end; i++ ){
   if(isPrimeNumber[i]){
     let m = 2;
     while(i * m <= end){
